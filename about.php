fc7bca4b115b0feffe90c4f3c556a93f561be8a8
Richard Peterson <webbuyrich@gmail.com>
2015-04-15 10:05:19 -0500

    created top nav, cover, picture, user links for about page

---
+++ about.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Facebook Mock-Up</title>
+        <link rel="shortcut icon" href="images/facebook.ico" type="image/x-icon" />
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="author" href="humans.txt">
+    </head>
+    <body>
+        <div class="row">
+          <div class="col-md-12 navbar-not-homepage">
+            <div class="col-md-1">&nbsp;</div>
+                <div class="col-md-10 no-padding">           
+                    <div class="col-md-3 no-left-padding">
+                        <img style="float:left;"src="http://placehold.it/35x35">
+                        <h1 id="logo-text">Facebook</h1> 
+                    </div>
+                    <div class="col-md-7 no-left-padding">                  
+                            <input type="text" class="form-control" id="inputSearch">                
+                    </div>
+                    <div class="col-md-2 text-right no-right-padding">
+                        <h3 id="user-name">Richard</h3>
+                    </div>
+                </div> 
+            <div class="col-md-1">&nbsp;</div>
+          </div>                        
+        </div>
+          
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+             <div class="col-md-1">&nbsp;</div>
+             <div class="col-md-10 no-padding" id="info-container">             
+                
+                    <img id="user-cover" src="http://placehold.it/1066x250">
+                    <img id="user-picture" src="http://placehold.it/150x150">
+                    <span id="user-name">Richard Peterson</span>
+                    <br />
+                    
+                        <div id="user-menu">
+                            <div class="col-md-3">&nbsp;</div>
+                            <div class="col-md-1 user-links">Timeline</div>
+                            <div class="col-md-1 user-links">About</div>
+                            <div class="col-md-1 user-links">Photos<span>(42)</span></div>
+                            <div class="col-md-1 user-links">Friends<span>(42)</span></div>
+                            <div class="col-md-5 user-links text-right"><a href="#">Edit Profile</a></div>
+                        </div>
+                    
+                
+
+             </div>
+             <div class="col-md-1">&nbsp;</div>             
+          </div>
+          
+        </div>
+        
+       
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
