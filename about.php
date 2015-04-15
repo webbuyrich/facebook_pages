4b5622fa2742f2fdcf6334f18d52147e8f6c7c71
Richard Peterson <webbuyrich@gmail.com>
2015-04-15 11:11:39 -0500

    added about section

--- about.php
+++ about.php
@@ -49,7 +49,59 @@
                             <div class="col-md-1 user-links">Friends<span>(42)</span></div>
                             <div class="col-md-5 user-links text-right"><a href="#">Edit Profile</a></div>
                         </div>
+                    <br />
                     
+                    <div id="about-menu">
+                        <div class="col-md-2 col-md-offset-5 about-links"><span id="about-text">About</span></div>
+                        <div class="col-md-2 col-md-offset-3 about-links"><button id="edit-profile">Edit your Profile</button></div>
+                        
+                    </div>
+                    
+                    
+                    <div id="about-menu-info">
+                    <div class="col-md-1">&nbsp;</div>
+                    <div class="col-md-4">
+                        
+                        <span id="basic-info">Basic Information</span>
+                        <br />
+                        
+                        <div class="col-md-4 no-left-padding">Birthday:</div>
+                        <div class="col-md-8 no-padding">January 26, 1980</div>
+
+                        <div class="col-md-4 no-left-padding">College:</div>
+                        <div class="col-md-8 no-padding">University of Texas at Brownsville</div>
+                       
+                        <div class="col-md-4 no-left-padding">Hometown:</div>
+                        <div class="col-md-8 no-padding">Alvin, TX</div>
+                        
+                        <div class="col-md-4 no-left-padding">Current Town:</div>
+                        <div class="col-md-8 no-padding">Manvel, TX</div>
+                        
+                        
+                        <div>&nbsp;</div>
+                        
+                        <span id="contact-info">Contact Information</span>
+                        <br />
+                        
+                        <div class="col-md-4 no-left-padding">Email:</div>
+                        <div class="col-md-8 no-padding">rlpeterson2212@gmail.com</div>
+
+                        <div class="col-md-4 no-left-padding">Telephone:</div>
+                        <div class="col-md-8 no-padding">281-244-8458</div>
+                        
+                        
+                    </div>
+                    
+                    <div class="col-md-1"></div>
+                    <div class="col-md-5">
+                    <span id="words-info">Words to Live By</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas doloremque illum suscipit, temporibus est doloribus, id officia quia facilis praesentium laboriosam laborum sint nemo optio molestias! Ipsa dicta doloribus mollitia.</p>
+                    <br />
+                    <span id="aboutme-info">About Me</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, ipsa illum excepturi vel hic quasi accusamus, perspiciatis numquam veritatis beatae, obcaecati vitae ex commodi illo neque natus sit reprehenderit? Repellat!</p>
+                    </div>
+                    <div class="col-md-1">&nbsp;</div>
+                    </div>
                 
 
              </div>
