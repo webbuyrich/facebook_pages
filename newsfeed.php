8edbb752e54fb63b052a186f6871953899a3f6b5
Richard Peterson <webbuyrich@gmail.com>
2015-04-15 13:51:29 -0500

    added newsfeed page

---
+++ newsfeed.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Facebook Mock-Up</title>
+        <link rel="shortcut icon" href="images/facebook.ico" type="image/x-icon" />
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="author" href="humans.txt">
+    </head>
+    <body>
+        <div class="row">
+          <div class="col-md-12 navbar-not-homepage">
+            <div class="col-md-1">&nbsp;</div>
+                <div class="col-md-10 no-padding">           
+                    <div class="col-md-3 no-left-padding">
+                        <img style="float:left;"src="http://placehold.it/35x35">
+                        <h1 id="logo-text">Facebook</h1> 
+                    </div>
+                    <div class="col-md-7 no-left-padding">                  
+                            <input type="text" class="form-control" id="inputSearch" placeholder="Search For Users">                
+                    </div>
+                    <div class="col-md-2 text-right no-right-padding">
+                        <h3 id="user-name">Richard</h3>
+                    </div>
+                </div> 
+            <div class="col-md-1">&nbsp;</div>
+          </div>                        
+        </div>
+          
+        </div>
+        <div class="col-md-12 no-padding">
+            
+            <div class="col-md-3 no-padding" id="active-news">
+                
+                <div class="row">
+                    <div class="col-md-11 col-md-offset-1">
+                        
+                            <img class="profile-picture" src="http://placehold.it/75x75" alt="">
+                       
+                            <div class="profile-name"><a href="">Michael Jordan</a><br /><a href="">Edit profile</a></div>
+                    </div>
+                    
+                </div>
+                <div class="col-md-12">&nbsp;</div>
+                <div class="row">
+                    <div class="col-md-11 col-md-offset-1">Recently Active</div>
+                </div>
+                <div class="row">
+                    <div class="col-md-11 col-md-offset-1">                        
+                        <img class="profile-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Jan 26 2013</p></div>
+                    </div>
+                    <div class="col-md-11 col-md-offset-1">                        
+                        <img class="profile-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Jan 26 2013</p></div>
+                    </div>
+                    <div class="col-md-11 col-md-offset-1">                        
+                        <img class="profile-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Jan 26 2013</p></div>
+                    </div>
+                    <div class="col-md-11 col-md-offset-1">                        
+                        <img class="profile-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Jan 26 2013</p></div>
+                    </div>
+                    <div class="col-md-11 col-md-offset-1">                        
+                        <img class="profile-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Jan 26 2013</p></div>
+                    </div>
+                    <div class="col-md-11 col-md-offset-1">                        
+                        <img class="profile-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Jan 26 2013</p></div>
+                    </div>
+                    <div class="col-md-11 col-md-offset-1">                        
+                        <img class="profile-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Jan 26 2013</p></div>
+                    </div>
+                    <div class="col-md-11 col-md-offset-1">                        
+                        <img class="profile-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Jan 26 2013</p></div>
+                    </div>
+                    <div class="col-md-11 col-md-offset-1">                        
+                        <img class="profile-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Jan 26 2013</p></div>
+                    </div>
+                    <div class="col-md-11 col-md-offset-1">                        
+                        <img class="profile-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Jan 26 2013</p></div>
+                    </div>
+                    <div class="col-md-11 col-md-offset-1">                        
+                        <img class="profile-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Jan 26 2013</p></div>
+                    </div>
+                    <div class="col-md-9 col-md-offset-3 ">                         
+                        <a href="">See all of your friends</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-9" id="main-news">
+                
+                <div class="col-md-8 col-md-offset-2 no-padding add-post" >
+                    <div id="post-header" class="text-center">Post(copied)</div>
+                    <div class="post-container" >
+                        <textarea type="textarea" placeholder="Tell the world something...." rows="4" cols="50" maxlength="50" id="insert-post" ></textarea>
+                    </div>
+                    <div id="post-submit" class="text-right"><button id="post-button" >Post</button></div>
+                </div>
+
+                <div class="col-md-8 col-md-offset-2 no-padding add-post" >
+                    
+                    <div class="post-container" >
+                        <img class="post-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Thursday Jan 26 2013</p></div>
+                        <div>
+                            <p class="post-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad inventore, sed libero iste. Impedit quibusdam dolorem ex autem cumque, reprehenderit tempora, earum, quae nobis pariatur enim doloribus. Dolores at, rerum.</p>
+                        </div>
+                    </div>
+                    <div class="comment-section">
+                        <div id="post-like"><a href="">Like</a></div>
+                        <div id="post-comment"><a href="">Comment</a></div>
+
+                    </div>
+                    
+                    
+                </div>
+
+                <div class="col-md-8 col-md-offset-2 no-padding add-post" >
+                    
+                    <div class="post-container" >
+                        <img class="post-picture" src="http://placehold.it/50x50" alt="">               
+                        <div class="profile-name"><a href="">Michael Jordan</a><br /><p>posted on Thursday Jan 26 2013</p></div>
+                        <div>
+                            <p class="post-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad inventore, sed libero iste. Impedit quibusdam dolorem ex autem cumque, reprehenderit tempora, earum, quae nobis pariatur enim doloribus. Dolores at, rerum.</p>
+                        </div>
+                    </div>
+                    <div class="comment-section">
+                        <div id="post-like"><a href="">Like</a></div>
+                        <div id="post-comment"><a href="">Comment</a></div>
+                        <br />
+                        <div id="post-user-like"><a href="">Michael Jordan</a> likes this</div>
+
+                    </div>
+                    
+                    
+                </div>
+
+            </div>
+        </div>
+        
+        
+       
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
